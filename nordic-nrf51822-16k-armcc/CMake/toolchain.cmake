551e50f31b92d15832055d3bef79ce3c1e22ed62
Rohit Grover <rohit.grover@arm.com>
2015-07-10 10:29:57 +0100

    COMMENT should come after commands.

--- nordic-nrf51822-16k-armcc/CMake/toolchain.cmake
+++ nordic-nrf51822-16k-armcc/CMake/toolchain.cmake
@@ -38,8 +38,8 @@ function(yotta_apply_target_rules target_type target_name)
             COMMAND fromelf --i32combined --output=${target_name}.hex ${target_name}
             # and append the softdevice hex file
             COMMAND srec_cat ${NRF51822_SOFTDEVICE_HEX_FILE} -intel ${target_name}.hex -intel -o ${target_name}-combined.hex -intel --line-length=44
-            COMMENT "hexifying and adding softdevice to ${target_name}"
             COMMAND srec_info ${target_name}-combined.hex -intel
+            COMMENT "hexifying and adding softdevice to ${target_name}"
             VERBATIM
         )
     endif()
